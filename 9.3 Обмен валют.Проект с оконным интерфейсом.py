acfd2df6e5a960668289660b784af1e22229dae7
Ольга <lakki717@gmail.com>
2024-10-26 11:37:56 +0300

    Создана функция exchange для получения из интернета курсов валют

--- 9.3 Обмен валют.Проект с оконным интерфейсом.py
+++ 9.3 Обмен валют.Проект с оконным интерфейсом.py
@@ -3,6 +3,29 @@ from tkinter import messagebox as mb
 import requests
 import json
 
+from bottle import response
+
+
+def exchange():
+    code = entry.get().upper()
+
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курсы обмена",f"Курс: {exchange_rate:.1f} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка",f"Валюта {code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка",f"Произошла ошибка: {e}.")
+
+    else:
+        mb.showerror("Внимание!","Введите код валюты")
+
 
 
 window = Tk()
@@ -14,7 +37,7 @@ Label(text="Введите код валюты").pack(padx=10, pady=10)
 entry = Entry()
 entry.pack(padx=10, pady=10)
 
-Button(text="Получить курс обмена к доллару", comand=exchange).pack(padx=10, pady=10)
+Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
 
 
 window.mainloop()
